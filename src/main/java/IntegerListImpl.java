43cb94520840d0d0a2b91568b7b0f5a6aa39f7da
V-I-K-I <anystrelchenko@gmail.com>
2022-08-12 15:14:41 +0300

    ДЗ Алгоритмы 3

--- src/main/java/IntegerListImpl.java
+++ src/main/java/IntegerListImpl.java
@@ -3,155 +3,144 @@ package main.java;
 import exceptions.ArrayIsFull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class IntegerListImpl implements IntegerList {
-    private ArrayList<Integer> integer;
+import static com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray.partition;
 
-    public IntegerListImpl() {
-        this.integer = new ArrayList<>(List.of(
-                new Integer("Каждый охотник"),
-                new Integer("желает знать"),
-                new Integer("где сидит"),
-                new Integer("фазан")
-        ));
+public class IntegerListImpl implements IntegerList {
+    private Integer[] integer;
+    private int size;
+
+    public IntegerListImpl(Integer[] integer) {
+        integer = new Integer[10];
+    }
+
+    public IntegerListImpl(int initSize) {
+        integer = new Integer[initSize];
     }
 
     @Override
     public Integer add(Integer item) {
-        Integer newInteger = new Integer(item);
-        integer.add(newInteger);
-        return newInteger;
+        growIfNeeded();
+        validateItem(item);
+        integer[size++]=item;
+        return item;
     }
 
     @Override
     public Integer add(int index, Integer item) {
-        int factSize = integer.size();
-        Integer newInteger = new Integer(item);
-        integer.add(index, newInteger);
-        if (integer.size() > factSize) {
-            throw new ArrayIsFull();
-        }
-        return newInteger;
+       validateItem(item);
+       growIfNeeded();
+       validateIndex(index);
+       if(index==size){
+           add(item);
+           return item;
+       }
+        System.arraycopy(integer,index,integer,index+1,size-index);
+       integer[index]=item;
+       return item;
     }
 
     @Override
     public Integer set(int index, Integer item) {
-        Integer newInteger = new Integer(item);
-        integer.set(index, newInteger);
-        if (index > integer.size()) {
-            throw new RuntimeException("Индекс вызодит за пределы массива");
-        }
-        return newInteger;
+        validateIndex(index);
+        validateItem(item);
+        integer[index]=item;
+        return item;
     }
 
     @Override
     public Integer remove(Integer item) {
-        Integer newInteger = new Integer(item);
-        if (integer.contains(newInteger)) {
-            integer.remove(newInteger);
-        } else {
-            throw new RuntimeException("Данной строки нет в листе");
-        }
-        return newInteger;
+      validateItem(item);
+      int index=indexOf(item);
+      if(index==-1){
+          throw new RuntimeException();
+      }
+      System.arraycopy(integer,index+1,integer,index,size-index);
+      size--;
+      return item;
     }
 
     @Override
     public Integer remove(int index) {
-        if (integer.get(index) != null) {
-            integer.remove(index);
-        } else {
-            throw new RuntimeException("Данного индеса нет в листе");
+        Integer item=integer[index];
+        validateIndex(index);
+        if(index==-size) {
+            System.arraycopy(integer, index + 1, integer, index, size - index);
         }
-        return integer.get(index);
+        size--;
+        return item;
     }
 
     @Override
     public boolean contains(Integer item) {
-        Integer[] copy=toArray();
+        Integer[] copy = toArray();
         sort(copy);
-        return binary(copy,item);
+        return binary(copy, item);
     }
 
     @Override
     public int indexOf(Integer item) {
-        Integer newInteger = toArray(item);
-        int indexOf = integer.indexOf(newInteger);
-        if (integer.contains(indexOf)) {
-            return indexOf;
+        for (int i = 0; i <size ; i++) {
+            Integer s=integer[i];
+            if(s.equals(item)){
+                return i;
+            }
         }
         return -1;
     }
 
+
     @Override
     public int lastIndexOf(Integer item) {
-        Integer newInteger = new Integer(item);
-        int indexOf = integer.lastIndexOf(newInteger);
-        if (integer.contains(indexOf)) {
-            return indexOf;
+
+        for (int i = size-1; i >= 0 ; i--) {
+            Integer s = integer[i];
+            if (s.equals(item)) {
+                return i;
+            }
         }
         return -1;
     }
 
     @Override
     public Integer get(int index) {
-        if (integer.get(index) != null) {
-            integer.get(index);
-        } else {
-            throw new RuntimeException("Данного индеса нет в листе");
-        }
-        return integer.get(index);
+       validateIndex(index);
+       return integer[index];
     }
 
     @Override
     public boolean equals(IntegerList otherList) {
-        List<Integer> newList = new ArrayList<>();
-        newList.add(new Integer("Каждый охотник"));
-        boolean result=integer.equals(newList);
-        if(newList==null){
-            throw new RuntimeException("Пустой лист");
-        }
-        return result;
-}
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
 
     @Override
     public int size() {
-        return integer.size();
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        if(integer.isEmpty()){
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
-        integer.removeAll(integer);
+        size = 0;
     }
 
     @Override
     public Integer[] toArray() {
-        Integer[] newInteger=new Integer[integer.size()];
-        for (int i = 0; i < integer.size(); i++) {
-            newInteger[i]=integer.get(i);
-        }
-        return newInteger;
+        return Arrays.copyOf(integer, size);
     }
-    private void sort(Integer[] arr){
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
+
+    private void sort(Integer[] arr) {
+        quickSort(arr,0,arr.length-1);
         }
     }
-    private boolean binary(Integer [] arr,Integer item){
+
+    private boolean binary(Integer[] arr, Integer item) {
         int min = 0;
         int max = arr.length - 1;
 
@@ -169,5 +158,58 @@ public class IntegerListImpl implements IntegerList {
             }
         }
         return false;
+    }
+
+    private void grow() {
+        integer = Arrays.copyOf(integer, size + size / 2);
+    }
+
+    private void validateItem(Integer item) {
+        if (item == null) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void growIfNeeded() {
+        if (size == integer.length) {
+            grow();
+        }
+    }
+
+    private void validateIndex(Integer index) {
+        if (index < 0 || index > size) {
+            throw new RuntimeException();
+        }
+    }
+    private void quickSort(Integer[]arr,int begin,int end){
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+
+    }
+
+    private int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+    }
+
+    private void swapElements(Integer[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
